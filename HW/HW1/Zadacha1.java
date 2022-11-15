1022414c3f01443825961294832d007055d847f5
Яхина Наталья Андреевна <  @vvrz.local>
2022-11-15 16:47:46 +0300

    Вычислить n-ое треугольного число, сумму, произвед

---
+++ HW/HW1/Zadacha1.java
@@ -0,0 +1,31 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! 
+// (произведение чисел от 1 до n)
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+package HW.HW1;
+
+import java.util.Scanner;
+
+public class Zadacha1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите число n: ");
+        int number = iScanner.nextInt();
+        iScanner.close();
+
+        int sum = 0;
+        for(int i = 0; i < number; i++) {
+            sum += i;
+        }
+
+        int mult = 1;
+        for(int i = 1; i <= number; i++) {
+            mult *= i; 
+        }
+        System.out.printf("Сумма чисел от 1 до %d -> %d", number, sum);
+        System.out.println();
+        System.out.printf("Произведение чисел от 1 до %d -> %d", number, mult);
+    }
+}
