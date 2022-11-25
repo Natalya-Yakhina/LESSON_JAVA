00dc6bb7847b4827a88c28ee6f7b6635c2a594aa
Яхина Наталья Андреевна <  @vvrz.local>
2022-11-25 13:19:28 +0300

    перевернутый список

--- HW/HW4/Zadacha1.java
+++ HW/HW4/Zadacha1.java
@@ -1,9 +1,24 @@
-// * 0.1) Написать метод, который принимает массив элементов, помещает их в стэк и выводит на консоль содержимое стэка.
-// * 0.2) Написать метод, который принимает массив элементов, помещает их в очередь и выводит на консоль содержимое очереди.
-
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 1. Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 package HW.HW4;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Zadacha1 {
-    
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<Integer>();
+        for(int i = 1; i < 5; i++){
+            list.add(i);
+        }
+        System.out.println("Исходный список: " + list);
+
+        LinkedList<Integer> listNew = new LinkedList<Integer>();
+        for(int i = list.size() - 1; i >= 0; i--){
+            listNew.add(list.get(i));
+        }
+        System.out.println("Перевернутый массив: " + listNew);
+    }
 }
