5424c9dfc89e2588a9d8ea6a64f33117572878e3
Яхина Наталья Андреевна <  @vvrz.local>
2022-11-25 14:06:46 +0300

    тел справочник

--- HW/HW3/Zadacha3.java
+++ HW/HW3/Zadacha3.java
@@ -7,33 +7,18 @@
 
 package HW.HW3;
 
+import java.util.List;
+
 public class Zadacha3 {
     
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("John", "33344");
+        phoneBook.add("Peter", "344");
+        phoneBook.add("Philipp", "344");
+        phoneBook.add("Philipp", "122");
+        
+        List<String> strings = phoneBook.get("Philipp");
+        System.out.println(strings);
+        }
 }
-
-
-// main() прописывая add() и get().
-
-// public class PhoneBook {
-
-// public void add(String number, String name) {
-// // TODO: 21.11.2022 Реализовать
-// }
-
-// public List<String> get(String name) {
-// // TODO: 21.11.2022 Реализовать получение всех номеров по фамилии
-// return null;
-// }
-
-// }
-
-//   public static void main(String[] args) {
-// PhoneBook phoneBook = new PhoneBook();
-// phoneBook.add("John", "33344");
-// phoneBook.add("Peter", "344");
-// phoneBook.add("Philipp", "344");
-// phoneBook.add("Philipp", "122");
-
-// List<String> strings = phoneBook.get("Philipp");
-// System.out.println(strings);
-// }
